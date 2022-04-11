ff45c05cfb40911a7657322d445a4eff4c4ff544
Lin Pei Wen <mobilesuit0922@gmail.com>
2022-04-11 15:13:50 +0800

    feat: 新增換頁後至頂

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,18 @@
   <router-view />
 </template>
 <script>
+import { useRouter } from "vue-router";
 export default {
   name: "App",
+  setup() {
+    const router = useRouter();
+    router.afterEach(() => {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+  },
 };
 </script>
